c93d0e20ea1e6c233ebb79721b277eebe4c1d3bd
Kostiantyn Skliar <skljar.k@gmail.com>
2019-03-21 23:42:42 +0200

    first commit

---
+++ src/main/java/SkljarKG38ExamProject/App.java
@@ -0,0 +1,13 @@
+package SkljarKG38ExamProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
